6fdc2f4ad15b84401d061968f098cbca26fea4a7
Payton Turner <paytont7819@gmail.com>
2022-11-03 11:36:47 -0600

    commit accounts

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,4 +1,4 @@
-"""djangp_project URL Configuration
+"""django_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# config/urls.py
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # new
+from django.views.generic.base import TemplateView  # new
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("accounts.urls")),  # new
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("articles/", include("articles.urls")),  # new
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("", include("pages.urls")),  # new
 ]
